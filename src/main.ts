981ef0e48dbb5120d122234bad233bf0efe6f439
youyacoder <liuyun@192.168.1.184>
2025-04-15 11:22:18 +0800

    feat: add sass

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import { installStore } from '@/stores'
 import { installRouter } from '@/router'
+import { installAssets } from '@/plugins/assets.ts'
 
 const app = createApp(App)
 installRouter(app)
 installStore(app)
+installAssets()
 app.mount('#app')
